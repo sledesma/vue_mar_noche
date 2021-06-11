3bc66ab1ab7393a3303e1c66f681714570277f3d
sledesma <sebas@Scorpion.sebas.com>
2021-06-10 21:12:39 -0300

    Mitad clase 2

---
+++ Clase2/ejemplo1/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <titulo v-bind:titulo="textoTitulo"></titulo>
+    <div id="content">
+      <div>Texto aqui</div>
+      <input type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+import Titulo from './Titulo.vue';
+
+export default {
+  components: {
+    Titulo
+  },
+  
+  data: function(){
+    return {
+      textoTitulo: "Vinclar input con div"
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+#content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#content div,
+#content input { 
+  width: 30%; 
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+#content input {
+  outline: none;
+  border: 0;
+  background: #CCC;
+  padding: 20px;
+}
+</style>
+
---
+++ Clase2/ejemplo1/src/Titulo.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ titulo }}</h1>
+</template>
+
+<script>
+export default {
+  props: ['titulo']
+}
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+}
+</style>
