8e667c0d363991edf94a59c58ad7e5471a0148ff
sledesma <sebas@Scorpion.sebas.com>
2021-06-22 20:51:32 -0300

    Será feo pero por lo menos funciona

--- Clase5/blog/src/App.vue
+++ Clase5/blog/src/App.vue
@@ -1,43 +1,17 @@
 <template>
-  <div class="maestro">
-    <!--Vista Maestro-->
-    <h2 class="titulo">Listado de posts</h2>
-    <ul class="lista">
-      <li>
-        <a href="#"
-          >sunt aut facere repellat provident occaecati excepturi optio
-          reprehenderit</a
-        >
-      </li>
-      <li>
-        <a href="#">qui est esse</a>
-      </li>
-      <li>
-        <a href="#"
-          >ea molestias quasi exercitationem repellat qui ipsa sit aut</a
-        >
-      </li>
-    </ul>
+  <div>
+    <mock></mock>
   </div>
 </template>
 
-<style>
-.maestro .titulo {
-  font-family: Arial, Helvetica, sans-serif;
-}
+<script>
+import Mock from "./views/Detalle.vue";
 
-.maestro .lista {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
+export default {
+  components: {
+    Mock,
+  },
+};
+</script>
 
-.maestro .lista li {
-  padding: 10px;
-}
-
-.maestro .lista li a {
-  text-decoration: none;
-  font-family: Arial, Helvetica, sans-serif;
-}
-</style>
+<style></style>
---
+++ Clase5/blog/src/views/Detalle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="detalle">
+    <div class="titulo">
+      sunt aut facere repellat provident occaecati excepturi optio reprehenderit
+    </div>
+    <div class="contenido">
+      quia et suscipit\nsuscipit recusandae consequuntur expedita et
+      cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
+      sunt rem eveniet architecto
+    </div>
+    <div class="info">Subido por: USUARIO 1</div>
+    <a href="#" class="atras">Atras</a>
+  </div>
+</template>
+
+<style scoped>
+.detalle {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.detalle .titulo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.detalle .contenido {
+  padding: 30px;
+  text-align: center;
+}
+
+.detalle .info {
+  float: right;
+}
+
+.detalle .atras {
+  display: inline-block;
+  text-align: center;
+  width: 100%;
+}
+</style>
