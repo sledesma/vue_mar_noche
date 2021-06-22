10f72bbdd2c6f98fb9acbbbc8e51f9880a0625b8
sledesma <sebas@Scorpion.sebas.com>
2021-06-22 20:38:46 -0300

    Maestro :: Estatica

---
+++ Clase5/blog/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="maestro">
+    <!--Vista Maestro-->
+    <h2 class="titulo">Listado de posts</h2>
+    <ul class="lista">
+      <li>
+        <a href="#"
+          >sunt aut facere repellat provident occaecati excepturi optio
+          reprehenderit</a
+        >
+      </li>
+      <li>
+        <a href="#">qui est esse</a>
+      </li>
+      <li>
+        <a href="#"
+          >ea molestias quasi exercitationem repellat qui ipsa sit aut</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.maestro .titulo {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.maestro .lista {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.maestro .lista li {
+  padding: 10px;
+}
+
+.maestro .lista li a {
+  text-decoration: none;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
